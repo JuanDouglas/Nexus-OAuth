624a59346af473d27bc90870a54d3935f779d64d
juandouglaspe <92160838+juandouglaspe@users.noreply.github.com>
2021-11-25 15:44:31 -0300

    Create project

---
+++ Nexus.OAuth.Server/Controllers/LoginController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Nexus.OAuth.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LoginController : ControllerBase
+    {
+    }
+}
