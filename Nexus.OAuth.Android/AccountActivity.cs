c417739be88049ce34d75369eb188ca4b8be266e
Juan Douglas <juandouglas2004@gmail.com>
2022-05-27 09:52:08 -0300

    Add anim fluid animation

--- Nexus.OAuth.Android/AccountActivity.cs
+++ Nexus.OAuth.Android/AccountActivity.cs
@@ -3,6 +3,7 @@ using Android.Content;
 using Android.OS;
 using Android.Runtime;
 using Android.Views;
+using Android.Views.Animations;
 using Android.Widget;
 using AndroidX.AppCompat.App;
 using AndroidX.Core.App;
@@ -19,6 +20,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Nexus.OAuth.Android
@@ -26,44 +28,25 @@ namespace Nexus.OAuth.Android
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
     public class AccountActivity : AuthenticatedActivity
     {
+        private View liquid;
         private TextView txtUser;
         private TextView txtEmail;
         private Button btnLogout;
         private RecyclerView rcvInfos;
         private AccountController accountController;
-        protected override async void OnCreate(Bundle savedInstanceState)
+        protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.activity_account);
-            if (!Authenticated)
-                return;
 
             txtUser = FindViewById<TextView>(Resource.Id.txtUser);
             txtEmail = FindViewById<TextView>(Resource.Id.txtEmail);
             btnLogout = FindViewById<Button>(Resource.Id.btnLogout);
             rcvInfos = FindViewById<RecyclerView>(Resource.Id.rcvInfos);
+            liquid = FindViewById(Resource.Id.imgLiquid);
 
-            Authentication auth = await Authentication.GetAsync(this);
-            accountController = new AccountController(this, auth);
-
-            AccountResult account = await accountController.MyAccountAsync();
-            string[] splName = account.Name.Split(' ');
-            Info[] infos = new Info[]
-            {
-                new Info(Resources.GetString(Resource.String.info_name), account.Name),
-                new Info(Resources.GetString(Resource.String.info_phone), account.Phone),
-                new Info(Resources.GetString(Resource.String.info_email), account.Email),
-                new Info(Resources.GetString(Resource.String.info_date_of_birth), account.DateOfBirth.ToLocalTime().ToString("dd/MM/yyyy")),
-                new Info(Resources.GetString(Resource.String.info_culture), new RegionInfo(CultureInfo.GetCultureInfo(account.Culture).LCID).DisplayName),
-                new Info(Resources.GetString(Resource.String.info_created), account.Created.ToLocalTime().ToString("dd/MM/yyyy HH:mm:ss"), true),
-            };
-
-            txtUser.Text = $"{splName[0]} {splName[1]}";
-            txtEmail.Text = account.Email;
-            btnLogout.Click += LogoutClick;
-            rcvInfos.AddItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.Vertical));
-            rcvInfos.SetLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.Vertical, false));
-            rcvInfos.SetAdapter(new InfosAdapter(infos));
+            Task ts = new Task(() => LoadAsync().Wait());
+            ts.Start();
         }
 
         private void LogoutClick(object sender, EventArgs args)
@@ -77,6 +60,53 @@ namespace Nexus.OAuth.Android
             await Authentication.LogoutAsync();
 
             await CheckLoginAsync(typeof(AccountActivity));
+        }
+
+        private async Task LoadAsync()
+        {
+            if (!Authenticated)
+                return;
+
+            Authentication auth = await Authentication.GetAsync(this);
+            accountController = new AccountController(this, auth);
+
+            AccountResult account = await accountController.MyAccountAsync();
+            string[] splName = account.Name.Split(' ');
+            RegionInfo region = new RegionInfo(CultureInfo.GetCultureInfo(account.Culture).LCID);
+            Info[] infos = new Info[]
+            {
+                new Info(Resources.GetString(Resource.String.info_name), account.Name),
+                new Info(Resources.GetString(Resource.String.info_phone), account.Phone),
+                new Info(Resources.GetString(Resource.String.info_email), account.Email),
+                new Info(Resources.GetString(Resource.String.info_date_of_birth), account.DateOfBirth.ToLocalTime().ToString("dd/MM/yyyy")),
+                new Info(Resources.GetString(Resource.String.info_culture), region.NativeName),
+                new Info(Resources.GetString(Resource.String.info_created), account.Created.ToLocalTime().ToString("dd/MM/yyyy HH:mm:ss"), true),
+            };
+
+            RunOnUiThread(() =>
+            {
+                txtUser.Text = $"{splName[0]} {splName[1]}";
+                txtEmail.Text = account.Email;
+                btnLogout.Click += LogoutClick;
+                rcvInfos.AddItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.Vertical));
+                rcvInfos.SetLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.Vertical, false));
+                rcvInfos.SetAdapter(new InfosAdapter(infos));
+            });
+
+            Animation anim = AnimationUtils.LoadAnimation(this, Resource.Animation.anim_fluid_account);
+            anim.AnimationEnd += (object sender, Animation.AnimationEndEventArgs args) =>
+           {
+               try
+               {
+                   liquid.Visibility = ViewStates.Gone;
+                   btnLogout.Visibility = ViewStates.Visible;
+               }
+               catch (Exception ex)
+               {
+               }
+           };
+
+            liquid.StartAnimation(anim);
         }
     }
 }
